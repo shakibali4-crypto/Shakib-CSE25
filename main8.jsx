9a6613d1d4e00dcd7bd87690cd0cd5ccf62714c6
Shakib Ali <shakibalihashmi4@gmail.com>
2026-06-01 10:58:24 +0530

    Add files via upload

---
+++ main8.jsx
@@ -0,0 +1,9 @@
+import React from "react"; 
+import ReactDOM from "react-dom/client"; 
+import App from "./app8"; 
+import "./app8"; 
+ReactDOM.createRoot(document.getElementById("root")).render( 
+  <React.StrictMode> 
+    <App /> 
+  </React.StrictMode> 
+); 
