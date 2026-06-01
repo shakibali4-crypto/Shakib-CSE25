7441215f0e5270aa72ef2c449821fb58815f0371
Shakib Ali <shakibalihashmi4@gmail.com>
2026-06-01 10:58:57 +0530

    Add files via upload

---
+++ main9.jsx
@@ -0,0 +1,9 @@
+import React from "react"; 
+import ReactDOM from "react-dom/client"; 
+import App from "./app9"; 
+import "./app9.css"; 
+ReactDOM.createRoot(document.getElementById("root")).render( 
+  <React.StrictMode> 
+      <App /> 
+  </React.StrictMode> 
+); 
